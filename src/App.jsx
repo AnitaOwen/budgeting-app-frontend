28acdc0306c1f10e79e8c2f23e82697df28bc718
AnitaOwen <anitaowen@pursuit.org>
2024-11-27 17:55:20 -0500

    added routes and form for testing login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+import { Routes, Route, useNavigate } from "react-router-dom";
 import './App.css'
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 
 function App() {
 
   return ( 
     <>
+    <Routes>
+      <Route path="/login" element={<Login />} /> 
+      <Route path="/register" element={<Register />} /> 
+      <Route path="/dashboard" element={<Dashboard />} /> 
+    </Routes>
     </>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+const Login = () => {
+    const navigate = useNavigate();
+    const URL = import.meta.env.VITE_BASE_URL;
+
+    const [user, setUser] = useState({ email: "", password: "" });
+  
+    function handleChange(event) {
+      setUser({ ...user, [event.target.id]: event.target.value });
+    }
+
+    // This function is being used in two places. It can be extracted to a helpers.js file
+    async function postFetch(user) {
+      console.log("URL:", URL);
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+  
+        body: JSON.stringify(user),
+      };
+  
+      try {
+        const res = await fetch(`${URL}/api/auth/login`, options);
+        
+        if (!res.ok) {
+          const errorMessage = await res.text(); 
+          alert(`Login failed: ${errorMessage}`);
+          setUser({ email: "", password: "" });
+          throw new Error("Login failed");
+        }
+        const data = await res.json();
+  
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+          // await setToggleLogin(true);
+          navigate("/dashboard")
+          console.log("JWT Login Success!")
+        } else {
+          console.log("JWT Login Failed");
+        }
+      } catch (error) {
+        console.error("Error during login:", error);
+      }
+    }
+  
+    // Login Function
+    async function handleSubmit(e) {
+      e.preventDefault();
+      if (!user.email|| !user.password) {
+        alert("You must enter an email and password");
+        return;
+      }
+      postFetch(user);
+    }
+  
+    //Demo User Login Function
+    async function handleDemoSignIn(e) {
+      e.preventDefault();
+      const user = { email: "demo@me.com", password: "password" };
+      postFetch(user);
+    }
+
+    return (
+        <div className="mt-5 container-sm text-center">
+          <h2 className="mt-4 mb-4">Welcome!</h2>
+          <button onClick={handleDemoSignIn} className="btn btn-dark btn-sm mb-4">Demo User</button>
+          <h4 className="mt-4">Login</h4>
+          <form onSubmit={handleSubmit} className="row justify-content-center">
+            <div className="col-md-6">
+              <div>
+                {/* <label htmlFor="username" className="form-label">
+                </label> */}
+                <input
+                  className="form-control text-center"
+                  id="email"
+                  value={user.email}
+                  type="text"
+                  placeholder="email"
+                  autoComplete="email"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-3 mt-3">
+                {/* <label htmlFor="password" className="form-label" >
+                </label> */}
+                <input
+                className="form-control text-center"
+                  id="password"
+                  type="password"
+                  placeholder="password"
+                  onChange={handleChange}
+                  autoComplete="current-password"
+                />
+              </div>
+            </div>
+            <div>
+              <button className="btn btn-info btn-sm mb-4">Submit</button>
+    
+            </div>
+            
+          </form>
+          <h5>
+            No Account? <Link to="/register">Register</Link>
+          </h5>
+        </div>
+      );
+    };
+
+export default Login;
