da5b7be4fb1c694a34539abf896b8e18d1ceb642
AnitaOwen <anitaowen@pursuit.org>
2024-11-27 19:26:32 -0500

    installed bootstrap and styled login form

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import './App.css'
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return ( 
     <>
+    <NavBar />
     <Routes>
       <Route path="/login" element={<Login />} /> 
       <Route path="/register" element={<Register />} /> 
       <Route path="/dashboard" element={<Dashboard />} /> 
+      {/* <Route path="*" element={<FourOFour />} /> */}
     </Routes>
     </>
   )
