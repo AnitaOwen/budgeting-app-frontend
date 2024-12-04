e19b13842eab6785b0b368aa38927d7255ba6ec1
AnitaOwen <anitaowen@pursuit.org>
2024-12-04 00:05:53 -0500

    added favicon icon to head in index.html

--- src/components/DisposableIncome.jsx
+++ src/components/DisposableIncome.jsx
@@ -1,22 +1,28 @@
-
+import { useState, useEffect } from "react";
 
 const DisposableIncome = ({transactions}) => {
+  const [disposableIncome, setDisposableIncome] = useState(0);
+
+  useEffect(() => {
     const calculateDisposableIncome = () => {
-        const totalIncome = transactions
+      const totalIncome = transactions
         .filter((transaction) => transaction.transaction_type === "income")
         .reduce((sum, curr) => sum + parseFloat(curr.amount), 0);
 
-    const totalExpenses = transactions
-      .filter((transaction) => transaction.transaction_type === "expense")
-      .reduce((sum, curr) => sum + parseFloat(curr.amount), 0);
+      const totalExpenses = transactions
+        .filter((transaction) => transaction.transaction_type === "expense")
+        .reduce((sum, curr) => sum + parseFloat(curr.amount), 0);
 
-    return totalIncome - totalExpenses;
-  };
+      return totalIncome - totalExpenses;
+    };
 
+    setDisposableIncome(calculateDisposableIncome());
+  }, [transactions]);
+  
   return (
     <div className="mt-4">
       <h4>Disposable Income</h4>
-      <p>${calculateDisposableIncome().toFixed(2)}</p>
+      <p>${disposableIncome.toFixed(2)}</p>
     </div>
   )
 }
